97c397f60a4e5947a444f0778255a3c27fbab083
SmirnovaAlena <smirnova.alenad@gmail.com>
2024-11-09 16:44:06 +0300

    Add files via upload

---
+++ Repetitions.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include<string>
+using namespace std;
+int main() {
+	string s;
+	cin >> s;
+	long long k = 1;
+	long long max = 0;
+	for (long long i = 0; i < s.size(); i++) {
+		if (s[i] == s[i + 1]) {
+			k++;
+
+		}
+		else {
+			if (k > max) {
+				max = k;
+			}
+			k = 1;
+		}
+	}
+	cout << max;
+	return 0;
+}

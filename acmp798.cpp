51a5255f0ce693486f31fe97690d9797a39491a5
SmirnovaAlena <smirnova.alenad@gmail.com>
2024-11-09 16:51:55 +0300

    Add files via upload

---
+++ acmp798.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, x, y, c;
+
+    cin >> a >> b >> x >> y >> c;
+    string c1 = "white";
+    string c0 = "black";
+    if (a * b % 2 == 0) {
+        cout << "equal";
+
+    }
+    else {
+        if ((x + y) % 2 == 0) {
+            if (c == 0) {
+                cout << c0;
+            }
+            if (c == 1) {
+                cout << c1;
+            }
+        }
+        else {
+            if (c == 1) {
+                cout << c0;
+            }
+            if (c == 0) {
+                cout << c1;
+            }
+
+        }
+    }
+    return 0;
+}

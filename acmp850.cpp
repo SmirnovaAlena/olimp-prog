9191c7e0bf5d2050289d5ebc771e7e8ce6baab7f
SmirnovaAlena <smirnova.alenad@gmail.com>
2024-11-09 16:53:22 +0300

    Add files via upload

---
+++ acmp850.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    int p, m;
+    cin >> p >> m;
+    int minp = p / 2 + p % 2;
+    int minm = m / 2 + m % 2;
+    int mino = minm;
+    if (minp > minm) {
+        mino = minp;
+    }
+    int maxm = m;
+    int maxp = p;
+    int maxo = maxm;
+    if (maxp < maxm) {
+        maxo = maxp;
+    }
+    cout << mino << " " << maxo << " ";
+
+    return 0;
+}

ea36195bf4fcec83544133127d25f1c5c2e54677
SmirnovaAlena <smirnova.alenad@gmail.com>
2024-11-09 16:41:57 +0300

    Add files via upload

---
+++ MissingNumber.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	long long n;
+	cin >> n;
+
+	long long a;
+	long long sum = 0;
+	for (long long i = 0; i < n - 1; i++) {
+		cin >> a;
+		sum += a;
+	}
+	cout << n * (n + 1) / 2 - sum;
+
+
+	return 0;
+}

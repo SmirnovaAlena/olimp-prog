bc68aa1721f62fe5a9e19c0adfb081b20a4016fc
SmirnovaAlena <smirnova.alenad@gmail.com>
2024-11-09 16:46:18 +0300

    Add files via upload

---
+++ IncreasingArray.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+	long n;
+	cin >> n;
+	long a = 0;
+	long b;
+	long sum = 0;
+	for (long i = 0; i < n; i++) {
+		b = a;
+		cin >> a;
+		if (a < b) {
+			sum += b - a;
+			a = a + (b - a);
+		}
+	}
+	cout << sum;
+	return 0;
+}
